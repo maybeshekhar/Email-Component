91ce47ae39f23f5ef3126d3753372a208d3c6f61
Chandrashekhar Jenamoni <shekharmaybe@gmail.com>
2023-04-26 21:07:20 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,66 @@
+export const mails = [
+    {
+      mId: 'guid-1',
+      unread: false,
+      isStarred: false,
+      subject: 'Medium Daily Digest',
+      content:
+        "Are you feeling stressed out and overwhelmed? Take a moment to pause and breathe. Mindful breathing can help you calm your mind and reduce stress. In this email, I'll share with you some simple breathing techniques you can try today. Let's start with a deep inhale... and exhale.",
+    },
+    {
+      mId: 'guid-2',
+      unread: true,
+      isStarred: false,
+      subject: 'Kotak',
+      content:
+        'Hi, Bank Account in 5 minutes, Zero balance bank account is waiting for you ',
+    },
+    {
+      mId: 'guid-3',
+      unread: true,
+      isStarred: true,
+      subject: 'Dominos Pizza India',
+      content:
+      "We're excited to offer you an exclusive deal on your next meal! Get a 50% discount on your order by using the promo code ZOMATO50 at checkout. Hurry, this offer is only available for a limited time. Order now and enjoy a delicious meal at half the price..",
+    },
+    {
+      mId: 'guid-4',
+      unread: true,
+      isStarred: false,
+      subject: 'LinkedIn',
+      content:
+        'You have just won the New York official learning ticket. Please click here.',
+    },
+    {
+      mId: 'guid-5',
+      unread: true,
+      isStarred: false,
+      subject: 'Zomato',
+      content:
+        "We're excited to offer you an exclusive deal on your next meal! Get a 50% discount on your order by using the promo code ZOMATO50 at checkout. Hurry, this offer is only available for a limited time. Order now and enjoy a delicious meal at half the price..",
+    },
+    {
+      mId: 'guid-6',
+      unread: true,
+      isStarred: false,
+      subject: 'Airtel',
+      content:
+        'Recharge done successfully!',
+    },
+    {
+      mId: 'guid-7',
+      unread: true,
+      isStarred: false,
+      subject: 'Instagram',
+      content:
+        'Congratulations !',
+    },
+    {
+      mId: 'guid-8',
+      unread: true,
+      isStarred: false,
+      subject: 'github',
+      content:
+        'New features in github, click here to leanr more about it.',
+    },
+  ]
---
+++ src/pages/Inbox.js
@@ -0,0 +1,41 @@
+import { Header } from "../components/Header";
+import { Filters } from "../components/Filters";
+import { useMails } from "../context/MailContext";
+import { Link } from "react-router-dom";
+
+export const Inbox = () => {
+    const {mailDispatch, filterCheckData, unRead} = useMails();
+    return (
+        <div className="inbox">
+            <Header />
+            <div className="content">
+                <h1><i class="fa-regular fa-envelope"></i> Mail Box</h1>
+                <Filters />
+                <h3 style={{textAlign: 'left', paddingLeft: '60px'}}>Unreads: {unRead}</h3>
+                <ul>
+                    {filterCheckData.map(mail => (
+                        <>
+                        <li key={mail.mId} style={{backgroundColor: mail.unread ? '#f5f5f4' : 'white', color: mail.unread ? 'gray' : 'black'}}>
+                            <div className="header">
+                                <h4>{mail?.subject}</h4>
+                                <button onClick={() => mailDispatch({type: 'star', payload: mail.mId})}><i style={{color: mail.isStarred ? 'rgb(253, 213, 15)' : 'rgb(198, 193, 193)'}} class="fa-solid fa-star" ></i></button>
+                            </div>
+                            <p>{mail?.content}</p>
+                            <div className="details">
+                                <Link to={`/details/${mail.mId}`}>View Details</Link>
+                                <div className='buttons'>
+                                    <button style={{color: 'red'}} onClick={() => mailDispatch({type: 'delete', payload: mail.mId})}><i class="fa-solid fa-trash-can"></i></button>
+                                    <button style={{color: 'orange'}} onClick={() => mailDispatch({type: 'markRead', payload: mail.mId})}>Mark as {mail.unread ? 'Unread' : 'Read'}</button>
+                                    <button style={{color: 'green'}}  onClick={() => mailDispatch({type: 'spam', payload: mail.mId})}>Report Spam</button>
+                                </div>
+                            </div>
+                            
+                        </li>
+                        <hr />
+                        </>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
